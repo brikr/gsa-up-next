097eafdd341b551e3d8df788c72a9c41914a1a05
Brik Royster <elkemore@gmail.com>
2018-12-03 20:01:24 -0800

    Add "Just finished"
    The event right before "Up next" will be labeled as "Just finished" if it's ended within the past hour
    Additionally, if all events are in the past, the final one will be labeled "Just finished"

--- src/app/times/times.component.ts
+++ src/app/times/times.component.ts
@@ -43,27 +43,50 @@ export class TimesComponent implements OnInit {
                 this.calendarService.getTodayEvents(params.get('apiKey')!)))
         .subscribe(res => {
           this.events = res;
-          this.updateFromNow();
+          this.updateStatus();
         });
 
     interval(1000).subscribe(() => {
-      this.updateFromNow();
+      this.updateStatus();
     });
   }
 
-  updateFromNow() {
+  // Updates the status map with new fromNow strings and status enums
+  // The fromNow strings for events should look like the following:
+  //  Past events: end of event .fromNow()
+  //  Most recently ended event:
+  //    If it has ended less than an hour ago (or has end date in the future):
+  //      "Just finished"
+  //    Otherwise: end of event.fromNow()
+  //  Next event: "Up next"
+  //  Other future events: start of event .fromNow()
+  // Whether an event is in the past or future is based on the *middle* of the
+  // event, not the start time. This allows us some room for early/late
+  // intermissions.
+  updateStatus() {
     let past = true;
-    this.events.forEach(e => {
+    this.events.forEach((e, i) => {
       const middle = moment.unix((e.start.unix() + e.end.unix()) / 2);
-      if (past && middle.isAfter()) {
-        // set the first future event to "Up next". Base this off the middle
-        // of each event so we don't see oddities with slightly early or late
-        // intermissions.
-        past = false;
-        this.eventStatus.set(e, {
-          fromNow: 'Up next',
-          status: FromNowStatus.UP_NEXT,
-        });
+      if (past) {
+        if (middle.isBefore()) {
+          this.eventStatus.set(e, {
+            fromNow: e.end.fromNow(),
+            status: past ? FromNowStatus.PAST : FromNowStatus.FUTURE,
+          });
+        } else {
+          past = false;
+          this.eventStatus.set(e, {
+            fromNow: 'Up next',
+            status: FromNowStatus.UP_NEXT,
+          });
+          if (i > 0 &&
+              this.events[i - 1].end.isAfter(moment().subtract(1, 'hour'))) {
+            this.eventStatus.set(this.events[i - 1], {
+              fromNow: 'Just finished',
+              status: FromNowStatus.PAST,
+            });
+          }
+        }
       } else {
         this.eventStatus.set(e, {
           fromNow: e.start.fromNow(),
@@ -71,5 +94,15 @@ export class TimesComponent implements OnInit {
         });
       }
     });
+    // if all events are in the past, label the last as just finished
+    // we don't have to worry about being within one hour here because this
+    // screen will show until the stream is done for the day, which isn't very
+    // long.
+    if (past) {
+      this.eventStatus.set(this.events[this.events.length - 1], {
+        fromNow: 'Just finished',
+        status: FromNowStatus.PAST,
+      });
+    }
   }
 }
